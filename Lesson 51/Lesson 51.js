3373fa57b1bf37a48809a830f8fe3d37bc99a470
Superbigboi <realkranda@gmail.com>
2023-10-15 16:53:05 +0100

    Lesson 51 - Sunday, October 15th, 2023
    i have lessons every sunday and saturday

--- Lesson 51/Lesson 51.js
+++ Lesson 51/Lesson 51.js
@@ -1,7 +1,28 @@
-let text = '{"employees":['
-    +'{"firstName":"John", "lastName":"Doe"},'
-    +'{"firstName":"Anna", "lastName":"Smith"},'
-    +'{"firstName":"Peter", "lastName":"Jones"}]}'
-  
- const staff = JSON.parse(text);
- console.log(staff.employees[0].lastName)
+// let text = '{"employees":['
+//     +'{"firstName":"John", "lastName":"Doe"},'
+//     +'{"firstName":"Anna", "lastName":"Smith"},'
+//     +'{"firstName":"Peter", "lastName":"Jones"}]}'
+
+//  const staff = JSON.parse(text);
+//  console.log(staff.employees[0].lastName)
+
+ let API_KEY = "5c153425fc7a9cc8dc8d0a56a667c50c";
+
+ const getWeatherData = (city) =>{
+    const url = "https://api.openweathermap.org/data/2.5/weather";
+    const Full_URL = `${url}?q=${city}&appid=${API_KEY}&units=imperial`;
+    const weatherpromise = fetch(Full_URL);
+    return weatherpromise.then((response) =>{
+        return response.JSON();
+    })
+ }
+
+ const searchCity = () => {
+    const city = document.getElementById('city-input').value;
+    getWeatherData(city).then((res)=>{
+        showWeatherData(res);
+    }).catch((error)=>{
+        console.log(error);
+        console.log('something when wrong');
+    })
+ }
---
+++ Lesson 51/Lesson 51.md
@@ -0,0 +1,1 @@
+yh this is the class with Mr. Job where I first decided to upload this stuff, and make
