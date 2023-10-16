3fe560f8ec58911c292d0b8dc195b78df02e4f1c
Superbigboi <realkranda@gmail.com>
2023-10-16 14:56:51 +0100

    Lesson 52 - Version 1
    yh this is my homework for the week, and it doesn't look good rn tbh

---
+++ Lesson 52/Lesson 52.js
@@ -0,0 +1,3 @@
+function remember(){
+    console.log("by the time this was made - One Piece episode 1079, Jujutsu Kaisen Season 2 episode 12, Bungo Stray Dogs Season 5, Black Clover episode 170, Toyko Revengers Season 2 episode 2 are the lastest episodes")
+}
