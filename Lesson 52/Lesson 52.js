480d5365a6dd0165d4f59ea6c1e0cc8f73299098
Superbigboi <realkranda@gmail.com>
2023-10-17 18:23:04 +0100

    Lesson 52 version 2
    i made it so that buttons will close some info since its a lot

--- Lesson 52/Lesson 52.js
+++ Lesson 52/Lesson 52.js
@@ -1,3 +1,29 @@
 function remember(){
     console.log("by the time this was made - One Piece episode 1079, Jujutsu Kaisen Season 2 episode 12, Bungo Stray Dogs Season 5, Black Clover episode 170, Toyko Revengers Season 2 episode 2 are the lastest episodes")
 }
+function HS() {
+    var section = document.getElementById("MI");
+    if (section.style.display === "none") {
+      section.style.display = "block";
+    } else {
+      section.style.display = "none";
+    }
+  }
+
+function IF(){
+    var section = document.getElementById("IF");
+    if (section.style.display === "none") {
+      section.style.display = "block";
+    } else {
+      section.style.display = "none";
+    }
+  }
+
+function fs(){
+    var fs = document.getElementById("fs");
+    if (fs.style.display === "none") {
+      fs.style.display = "block";
+    } else {
+      fs.style.display = "none";
+    }
+  }
