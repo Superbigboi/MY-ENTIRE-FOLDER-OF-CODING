19c17f6fef3d0cc0891f6111b9921ed696889882
Superbigboi <realkranda@gmail.com>
2023-10-28 15:59:17 +0100

    Lesson 55 (again)
    im still not done with class

--- Lesson 55/Lesson 55.js
+++ Lesson 55/Lesson 55.js
@@ -39,20 +39,31 @@ const boss = {
     fname:"Jacob",
     lname:"Garcia",
     age:50,
+    location:"Abuja" ,
     cars:{
         car1:"Camery",
         car2:"Honda",
         car3:"Lexus",
     },
     mcars:["Honda","Nissan", "Toyota" ],
-    favorites:[
-        favorite1="One Piece",
-        favorite2="Basketball",
-        favortite3="Fortnite"],
+    favorites:{
+        favorite1 : "One Piece",
+        favorite2 : "Basketball",
+        favortie3 : "Fortnite"
+    },
     FullName: function(){
-        return this.fname+""+this.lname
-    }
+        return this.fname+" "+this.lname;
+    },
+    Message: function(){
+        return "Hello "+this.fname+", you are "+this.age+" years old, and your last name is " + 
+        this.lname+" you live in "+this.location+" your favorite sport is "+ this.favorites.favorite2+ 
+        " and one of cars is a "+ this.mcars[0]
+    },
 }
 console.log(boss)
 console.log(boss.cars.car3)
-console.log(cars[0])
+console.log(boss.mcars[0])
+console.log(boss.FullName())
+console.log(boss.Message())
+
+
