d9c145f5e5e9d842fdd57fad8b1ca240dc3269a2
Superbigboi <realkranda@gmail.com>
2023-12-24 15:05:09 +0100

    Lesson 67 - Sunday, December 24th, 2023

---
+++ Lesson 67/Lesson 67.js
@@ -0,0 +1,7 @@
+// $("span").parentsUntil("div").css("color","blue") /* */
+// $("span").css("color","black")
+// $("div").children().css("color","brown")
+// $("div").children("p.first").css({"background-color":"green","color":"red"})
+// $("div").find("span").css("color","orange")
+$("section").next().css("color","red")
+$("")
