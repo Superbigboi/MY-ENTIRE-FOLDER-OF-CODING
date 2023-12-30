bd2172fb22f65957d14d0b7316e66460b9d5d88b
Superbigboi <realkranda@gmail.com>
2023-12-30 16:21:34 +0100

    Lesson 68 - Sunday, December 30th, 2023
    yeah, tomorrow is christmas eve

---
+++ Lesson 68/Lesson 68.js
@@ -0,0 +1,5 @@
+$("div").first().css("background-color","red")
+$("div").last().css({"background-color":"black","color":"white"})
+$("div").eq(1).css({"background-color":"green","color":"yellow"})
+$("div").filter("#container2").css({"background-color":"pink","color":"green"})
+// $("h1").not("#h").css("bs","2px solid black")
